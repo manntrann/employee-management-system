e5b4c6d1f160e75b0b48d6fe13c95ec21936d9d9
manntrann <tranbichman2000@gmail.com>
2026-05-21 17:51:57 +0700

    Create CRUD APIs and connection to database

---
+++ EmployeeManagement.API/Controllers/EmployeesController.cs
@@ -0,0 +1,77 @@
+﻿using EmployeeManagement.API.Data;
+using EmployeeManagement.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagement.API.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class EmployeesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public EmployeesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployees()
+        {
+            return await _context.Employees.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> GetEmployee( int id )
+        {
+            var employee = await _context.Employees.FindAsync( id );
+
+            if(employee == null)
+            {
+                return NotFound();
+            }
+            return employee;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Employee>> CreateEmployee(Employee employee)
+        {
+            _context.Employees.Add( employee );
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(
+                nameof(GetEmployee),
+                new { id = employee.Id },
+                employee);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateEmployee (int id, Employee employee)
+        {
+            if(id != employee.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(employee).State = EntityState.Modified; ;
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteEmployee(int id)
+        {
+            var employee = await _context.Employees.FindAsync ( id );
+
+            if(employee == null)
+            {
+                return NotFound();
+            }
+
+            _context.Employees.Remove(employee);
+            await  _context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
---
+++ EmployeeManagement.API/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagement.API.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Position { get; set; }
+
+        public decimal Salary { get; set; }
+    }
+}
